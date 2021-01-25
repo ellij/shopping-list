0b04825220edaef19d7889f283512f9a365f3329
ellij <elli.jokinen@gmail.com>
2021-01-25 17:54:27 +0200

    Shopping List valmis

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList} from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+  
+  const add = () =>{
+    setData([...data, {key:text}]);
+    setText('');
+  }
+
+  const clear = () =>{
+    let empty = [''];
+    setData(...empty);
+    setText('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>     
+      <TextInput style={styles.textInput}
+        onChangeText={text => setText(text)} 
+        value={text}
+      />
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+        <Button color="#065535" onPress={add} title="ADD" />
+        <Button color="#065535" onPress={clear} title="CLEAR" />
+      </View> 
+      <Text style={styles.text}>Shopping List</Text> 
+      <FlatList 
+        data={data}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />   
     </View>
   );
 }
@@ -15,7 +39,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center', 
+    padding: 50, 
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#800080',
+  },
+  textInput: {
+    height: 40, 
+    width: "50%", 
+    borderColor: '#808080', 
+    borderWidth: 2, 
+    margin: 5,
+    fontSize: 18,    
+  },  
 });
